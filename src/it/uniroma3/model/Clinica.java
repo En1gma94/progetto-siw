390e563452f704da3af7e21c0f9ffae7a52576e4
Stefano <stefano.baruffa@gmail.com>
2016-05-25 14:13:21 +0200

    Primo commit

---
+++ src/it/uniroma3/model/Clinica.java
@@ -0,0 +1,13 @@
+package it.uniroma3.model;
+
+public class Clinica {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/it/uniroma3/model/Prerequisito_esame.java
@@ -0,0 +1,26 @@
+package it.uniroma3.model;
+
+public class Prerequisito_esame {
+private long id;
+private String nome, descrizione;
+
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getDescrizione() {
+	return descrizione;
+}
+public void setDescrizione(String descrizione) {
+	this.descrizione = descrizione;
+}
+
+}
